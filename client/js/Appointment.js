8d654872cd6b10482ae99fbaa037a118e69bac9b
Bailey Nguyen <bailey.tan.nguyen@gmail.com>
2021-02-28 05:02:42 -0600

    Adding Node.js
    Attempting to run webapp on Node.js
    Keep in different branch until it actually works.

---
+++ client/js/Appointment.js
@@ -0,0 +1,20 @@
+export default class Appointment {
+    constructor(date, student, timeLength) {
+        this.date = date;
+        this.student = student;
+        this.timeLength = timeLength;
+    }
+
+    get getDate() {
+        return this.date;
+    }
+
+    get getStudent() {
+        return this.student;
+    }
+
+    get getTimeLength() {
+        return this.timeLength
+    }
+    
+}
