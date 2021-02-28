1e65f4831c039df6de02b362fd3a75b492462f0b
Bishesh Pote <potebishesh@gmail.com>
2021-02-27 21:59:23 -0600

    disable calendar added

--- js/main.js
+++ js/main.js
@@ -13,3 +13,28 @@ function sayHello() {
 
     console.log(`Hello ${fname} ${lname}`);
 }
+
+
+
+// Disable some dates ****************
+document.getElementById('appdate').onchange = function(e){
+    var tempDate = new Date(e.target.value);
+
+    const currDate = new Date();
+    var nextDay = new Date();
+    var nextTwoWeek = new Date();
+
+    nextDay.setDate(currDate.getDate() + 1);
+
+    if(tempDate.getDay() == 5 || tempDate.getDay() == 6)
+    {
+        alert('No appointments available for weekends.');
+        e.target.value = '';
+    }
+    else if((tempDate.getDate() < nextDay.getDate()) && (tempDate.getMonth() <= currDate.getMonth()) )
+    {
+        alert('Appointment booking available only from next day.');
+        e.target.value = '';
+    }
+};
+// ****************
