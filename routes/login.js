488d1ab2b864c154a75be3b07ffc451a773f9923
Tyler Westbrook <twestbrook09@live.com>
2021-03-21 10:00:54 -0500

    tried to use the Router class but couldn't get it to work. Currently not used but pushing changes anyways.

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,27 @@
 "use strict";
 const express = require("express");
 let router = express.Router();
+// const passport = require("passport");
 
-router.get('/', (req, response) => {
+router.get('/login', (req, response) => {
     response.render('instructor_login.html');
-});
-router.post('/', async (req, response) => {
+})
 
-});
+// router.get('/login', checkNotAuthenticated, (req, response) => {           //checkNotAuthenticated for if user is already logged in, don't go back to login page
+//     response.render('instructor_login.html');
+// });
+
+// function checkNotAuthenticated(req, response, next) {
+//     if(req.isAuthenticated()) {
+//         return response.redirect('/instructor_main');
+//     }
+
+//     next();
+// }
+
+// router.post('/login', checkNotAuthenticated, passport.authenticate('local', {  //checkNotAuthenticated for if user is already logged in, don't re-log in
+//     successRedirect: '/instructor_main',
+//     failureRedirect: '/login',
+//     failureFlash: true
+// }));
 module.exports = router;
