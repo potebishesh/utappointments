bc4b81956c08d5a79813e4b46b961ccc8eba709e
Bishesh Pote <58003718+pote-create@users.noreply.github.com>
2021-03-24 20:38:14 -0500

    added navigation in instructor pages and set up about page

--- routes/contact.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const express = require("express");
-let router = express.Router();
-
-router.get('/', (req, response) => {
-    response.render('contact.html');
-});
-
-module.exports = router;
