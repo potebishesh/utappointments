58a2052992786e7c1dc435200f1238957a361619
Bishesh Pote <58003718+pote-create@users.noreply.github.com>
2021-04-10 15:02:38 -0500

    added display time after entering date and display available days only in the calendar

--- public/js/make_appointment.js
+++ public/js/make_appointment.js
@@ -6,11 +6,7 @@ import {to24hour} from './utilities.js'
 import {dateYY} from './utilities.js'
 //const { response } = require("express");
 
-
-getAvailability();
-
 //initiate all data on form
-
 var form = document.getElementById("myform");
 var button = document.getElementById("submit");
 
@@ -19,33 +15,15 @@ var lname = document.getElementById("lname");
 var email = document.getElementById("email");
 var date = document.getElementById("datepicker");
 var time = document.getElementById("time");
+var time_label = document.getElementById("time_label");
+var error = document.getElementById("displayError");
 
-// Ensure date is selected before time
-time.addEventListener("focus", function()
-{
-    if(!date.value){
-        alert('Please select date before time.');
-        date.focus();
-        time.value = ' ';
-    }
-    
-    jQueryTime('#time').timepicker({
-        minTime: '8:00am',
-        maxTime: '11:30pm',
-        interval:'10'
-    }); 
+var disabled_dates = [];
+var availability = [];
+getAvailability();
 
-}
-);
-
-date.addEventListener("focus", function(){
-    time.value = ' ';
-});
-// ***************************************
-
-
+email.addEventListener("focusout", check_email(email.value));
 button.addEventListener("click", checkKeys);
-
 
 function bookAppointment() {
     var fname_ = fname.value;
@@ -102,52 +80,66 @@ function bookAppointment() {
 }
 
 
-// Disable some dates ****************
-/*
-document.getElementById('date').onchange = function(e){
-    var tempDate = new Date(e.target.value);
-
-    const currDate = new Date();
-    var nextDay = new Date();
-    var nextTwoWeek = new Date();
-
-    nextDay.setDate(currDate.getDate() + 1);
-
-    if(tempDate.getDay() == 5 || tempDate.getDay() == 6)
-    {
-        alert('No appointments available for weekends.');
-        e.target.value = '';
-    }
-    else if((tempDate.getDate() < nextDay.getDate()) && (tempDate.getMonth() <= currDate.getMonth()) )
-    {
-        alert('Appointment booking available only from next day.');
-        e.target.value = '';
-    }
-};
-
+/*  enable_date() function sets up the date input field in the webpage.
+    The date field has minimum date of today + 2 days and maximum date of
+    today + 15 days. testDate variable increases from minDate to maxDate
+    in the for loop. If it doesn't match with any of the available days,
+    it stores the date in global array disabled_dates. Disabled dates are
+    grayed in the calendar UI using "beforeShowDay: DisableDates".
 */
-
-// Using jquery datepicker to enable only 2 weeks of date
-
-jQuery(document).ready(function() {
+function enable_date() {
     let today = new Date();
     let minDate = new Date();
     minDate.setDate(today.getDate() + 2)
     let maxDate = new Date();
     maxDate.setDate(today.getDate() + 15);
     
+    var testdate = new Date();
+    // initialized at -1 because the loop has +1
+    testdate.setDate(minDate.getDate() - 1);
+
+    for(var i = 0; i < 15; i++){
+        testdate.setDate(testdate.getDate() + 1);
+        var found = false;
+        for(var j = 0; j < availability.length; j++){
+            if(testdate.getDay() == availability[j][0]){
+                found = true;
+            }
+        }
+        if(!found){
+            var day = testdate.getDate().toString().padStart(2, '0');
+            var month = (testdate.getMonth() + 1).toString().padStart(2, '0');
+            var year = testdate.getFullYear();
+            var temp = month + '-' + day + '-' + year;
+            disabled_dates.push(temp);
+        }
+    }
+
 	jQuery('#datepicker').datepicker({
 		minDate: minDate,
 		maxDate: maxDate,
+        beforeShowDay: DisableDates,
 		dateFormat: 'mm-dd-yy',
-		constrainInput: true
-	});
-});
+		constrainInput: true,    
+	}).on("change", function(){
+        enable_time(date.value);
+    });
 
-//**************************************************
+    function DisableDates(date) {
+        for(var i = 0; i < disabled_dates.length; i++){
+            var tempDate = disabled_dates[i].split("-");
+            if (date.getFullYear() == tempDate[2]
+            && date.getMonth() == tempDate[0] - 1
+            && date.getDate() == tempDate[1]) {
+                return [false];
+          }
+        }
+       return [true];
+    }
+};
+
 
 // Pop up for office hours
-
 jQuery(window).load(function () {
     jQuery(".trigger_popup_fricc").click(function(){
        jQuery('.hover_bkgr_fricc').show();
@@ -160,8 +152,8 @@ jQuery(window).load(function () {
     });
 });
 
-//**************************************************
 
+// API call to get office hours, after completion, load the pop up HTML
 function getAvailability(){
     fetch('http://localhost:5000/getAvailability')
     .then(response => response.json())
@@ -169,6 +161,9 @@ function getAvailability(){
     });
 }
 
+
+// loads HTML, stores information in global array availability[day, start_time, end_time]
+// enable_date()
 function loadHTMLTable(data) {
     
     const table = document.querySelector('table tbody');
@@ -180,20 +175,22 @@ function loadHTMLTable(data) {
     let tableHTML = "";
 
     data.forEach(function ({day, start_time, end_time}){
-        day = numToDay(day);
+        
         start_time = to12hour(start_time);
         end_time = to12hour(end_time);
+        availability.push([day, start_time, end_time]);
+        day = numToDay(day);
         tableHTML += "<tr>";
         tableHTML += `<td>${day}</td>`;
         tableHTML += `<td>${start_time}</td>`;
         tableHTML += `<td>${end_time}</td>`;
         tableHTML += "</tr>";
     });
-
     table.innerHTML = tableHTML;
+
+    enable_date();
 }
 
-// **************************************************
 
 // Checks if key matches
 function checkKeys(){
@@ -221,4 +218,38 @@ function checkKeys(){
             return alert('Key does not match. Retry or contact instructor.');
         }       
     });
+}
+
+
+
+function enable_time(date){
+    if(date){
+        jQueryTime('#time').timepicker({
+            minTime: availability[0][1],
+            maxTime: availability[0][2],
+            interval:'10'
+        }); 
+        time.style.display = "block";
+        time_label.style.display = "block";
+    }
+    else{
+        time.style.display = "none";
+        time_label.style.display = "none";
+    }
+}
+
+
+function check_email(email_){
+    var domain = email_.split('@');
+    if(domain.length == 2){
+        if(domain[1] != 'mavs.uta.edu'){
+            error.style.display = "block";
+            error.innerHTML = "Email should be mavs.uta.edu";
+            email.focus();
+        }
+    }
+    else if(domain.length >=3){
+        error.style.display = "block";
+        error.innerHTML = "Enter valid email";
+    }
 }
