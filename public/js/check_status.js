1f30395deea6d356076ac373eafc38fa92b05fd8
Bishesh Pote <58003718+pote-create@users.noreply.github.com>
2021-04-26 00:56:26 -0500

    added add to calendar in check status

--- public/js/check_status.js
+++ public/js/check_status.js
@@ -1,7 +1,15 @@
+
+import {to12hour} from './utilities.js'
+
 var button = document.getElementById("submit");
 
 var reference = document.getElementById("reference");
 var email = document.getElementById("email");
+var displayBox = document.getElementById("displayBox");
+var message = document.getElementById("message");
+var start = document.getElementById("start");
+var end = document.getElementById("end");
+var description = document.getElementById("description");
 
 button.addEventListener("click", checkStatus);
 
@@ -12,22 +20,45 @@ function checkStatus(){
 function loadHTMLTable(data) {
     const table = document.querySelector('table tbody');
     if(data.length === 0) {
-        table.innerHTML = "<tr><td class='no-data' colspan='5'> No Appointment Found </td></tr>";
+        message.innerHTML = "No such appointment exists.";
+        displayBox.style.display = "none";
         return;
     }
-    
+
+    message.innerHTML = "";
     let tableHTML = "";
     data.forEach(function ({ref_num, st_fname, st_lname, app_date, app_time}){
+
+        let date = new Date(app_date);
+        var day = date.getDate().toString().padStart(2, '0');
+        var month = (date.getMonth() + 1).toString().padStart(2, '0');
+        var year = date.getFullYear();
+        date = month + '/' + day + '/' + year;
+        
+        let time = to12hour(app_time);
+
+        var tempdate = new Date(date + " " + time);
+        tempdate.setMinutes(tempdate.getMinutes() + 10);
+
+        var endtime = tempdate.getHours() + ":" + tempdate.getMinutes().toString().padStart(2, '0') + ":" + tempdate.getSeconds().toString().padStart(2, '0');
+       
+
+        start.innerHTML = date + " " + time;
+        end.innerHTML = date + " " + endtime;
+        description.innerHTML = "Reference Number: " + ref_num;
+
         tableHTML += "<tr>";
         tableHTML += `<td>${ref_num}</td>`;
         tableHTML += `<td>${st_fname}</td>`;
         tableHTML += `<td>${st_lname}</td>`;
-        tableHTML += `<td>${app_date}</td>`;
-        tableHTML += `<td>${app_time}</td>`;
+        tableHTML += `<td>${date}</td>`;
+        tableHTML += `<td>${time}</td>`;
         tableHTML += "</tr>";
     });
 
     table.innerHTML = tableHTML;
+    displayBox.style.display = "block";
+    
 }
 
 function getAppointmentStatus(reference, email){
